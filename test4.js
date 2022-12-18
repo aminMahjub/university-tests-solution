ae3469a93dfe4a40e7c6853acef5db182e3881b2
Mmmad Amin Mahjub Asl <aminmahjoob3172@gmail.com>
2022-12-18 06:15:25 +0330

    Found test 4 solution

---
+++ test4.js
@@ -0,0 +1,9 @@
+// write a program that gets the number and returns even and odd of the number's digit
+
+const number = 456758793;
+let convertToArray = Array.from(String(number), Number);
+
+for (let digit of convertToArray) {
+    if (digit % 2 === 0) console.log(`Even digit: ${digit}`);
+    else console.log(`Odd digit: ${digit}`);
+}
