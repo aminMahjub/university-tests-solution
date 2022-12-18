5a99ac49f8cf03e46595ec540c482d0d44a33a3f
Mmmad Amin Mahjub Asl <aminmahjoob3172@gmail.com>
2022-12-18 06:08:02 +0330

    Found test 3 solution

---
+++ test3.js
@@ -0,0 +1,12 @@
+//write a program that returns reverse of the number
+
+const number = 12323;
+let convertToArray = Array.from(String(number));
+let result = Array.from(String(number));
+let j = 0;
+
+for (let i = 0; i < convertToArray.length; i++) {
+    result[i] = convertToArray[convertToArray.length - ++j];
+}
+
+console.log(result.join(''));
