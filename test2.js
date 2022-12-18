6ee8b10b78d1d79543dedf4c595a515df0d994c6
Mmmad Amin Mahjub Asl <aminmahjoob3172@gmail.com>
2022-12-18 05:41:20 +0330

    Found test 2 solution

---
+++ test2.js
@@ -0,0 +1,7 @@
+// write a program that returns length of the numbers
+
+// the input is an example
+const number = 123;
+let convertToString = number.toString();
+
+console.log(convertToString.length);
